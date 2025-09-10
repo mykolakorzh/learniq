2846b5a76bef6d2beeea6b30c6b498bbb7a4d02f
Mykola Korzh <114113283+mykolakorzh@users.noreply.github.com>
2025-09-10 12:52:22 +0200

    Refactor app.dart for consistency in string formatting
    - Changed double quotes to single quotes for string literals in app.dart for uniformity.
    - Ensured proper formatting of the initial route string.
    - Added a newline at the end of the file for better code practices.

--- learniq/lib/app.dart
+++ learniq/lib/app.dart
@@ -1,6 +1,6 @@
-import "package:flutter/material.dart";
-import "core/theme/app_theme.dart";
-import "core/routing/app_router.dart";
+import 'package:flutter/material.dart';
+import 'core/theme/app_theme.dart';
+import 'core/routing/app_router.dart';
 
 class LearniqApp extends StatelessWidget {
   const LearniqApp({super.key});
@@ -8,11 +8,11 @@ class LearniqApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "LearnIQ",
+      title: 'LearnIQ',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       onGenerateRoute: AppRouter.generateRoute,
-      initialRoute: "/",
+      initialRoute: '/',
     );
   }
 }
