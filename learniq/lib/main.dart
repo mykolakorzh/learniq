9ed9c02d657ee5463ccd28277406824914f599ca
Mykola Korzh <114113283+mykolakorzh@users.noreply.github.com>
2025-09-10 12:52:50 +0200

    Refactor main.dart for consistency in string formatting
    - Changed double quotes to single quotes for string literals in main.dart for uniformity.
    - Ensured proper formatting and structure of the main function.
    - Added a newline at the end of the file for better code practices.

--- learniq/lib/main.dart
+++ learniq/lib/main.dart
@@ -1,6 +1,6 @@
-import "package:flutter/material.dart";
-import "package:flutter/services.dart";
-import "app.dart";
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
